f00d26dd5879fce431000437d9da5b466a69dcb8
MatteoFalcioni <matteo.falk@gmail.com>
2025-09-08 15:53:38 +0200

    main loop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.make_graph import get_builder
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain_core.messages import HumanMessage
 from dotenv import load_dotenv
+import uuid
 
 if __name__ == "__main__":
 
@@ -12,6 +13,24 @@ if __name__ == "__main__":
 
     graph = builder.compile(checkpointer=memory)
 
-    init = {"messages": [HumanMessage(content="Hello, Write python code to compute sin(pi/4)")]}
-    out = graph.invoke(init, config={"configurable" : {"thread_id": 1}})
-    print(out["messages"][-1].content)
+    convo_id = str(uuid.uuid4())[:8]
+
+    print("=== Type /bye to exit. ===\n")
+
+    usr_msg = ""
+
+    while True:
+
+        usr_msg = input("User: ")
+
+        if "/bye" in usr_msg.lower():
+            break
+
+        result = graph.invoke(
+            {"messages": [{"role": "user", "content": usr_msg}]},
+            {"configurable": {"thread_id": f"{convo_id}"}, "recursion_limit" : 25},
+        )
+
+        ai_message = result["messages"][-1].content
+
+        print(f'\AI: {ai_message}\n')
